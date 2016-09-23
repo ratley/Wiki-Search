c9b796b8311a9cb720981b17cc3174cf25223a74
[01;31m[K"name"[m[K: "bradley inniss <[01;31m[K"email"[m[K: null>
2016-09-22 23:14:41 -0700

    Created WikiSearch module and transfer methods to it

--- spec/wiki_spec.rb
+++ spec/wiki_spec.rb
@@ -1,17 +1,19 @@
-require_relative 'wiki_search'
+require_relative '../wiki_search'
 
 describe WikiSearch do
-let(:string) { "dev bootcamp"}
-  it 'replaces spaces in string with underscores' do
+  let(:string) { "dev bootcamp" }
+  let(:link) { 'https://en.wikipedia.org/wiki/' }
+
+  it "replaces spaces in string with underscores" do
     expect(WikiSearch.to_underscore(string)).to eq('dev_bootcamp')
   end
 
+  it "adds string to wikipedia link" do
+    expect(WikiSearch.link(string)).to eq("https://en.wikipedia.org/wiki/dev_bootcamp")
+  end
 
-
-
-
-
-
-
+  it "parses page" do
+    expect(WikiSearch.parse_page).to be_a Nokogiri::HTML::Document
+  end
 
 end
--- wiki_search.rb
+++ wiki_search.rb
@@ -7,64 +7,80 @@ require 'open-uri'
 
 module WikiSearch
 
+  @@wiki = "https://en.wikipedia.org/wiki/"
+
   def self.to_underscore(string)
     string.gsub(/ /, '_')
   end
 
+  def self.link(string)
+    @@wiki + to_underscore(string)
+  end
+
+  def self.parse_page
+    page = Nokogiri::HTML(open(link))
+    @@header = page.css('h1')[0].text
+    @@description = page.css("p")[0].text
+    page
+  end
+
+  def self.display
+    @@header.each_char do |chr|
+        print chr
+        sleep(0.1)
+        STDOUT.flush
+      end
+      puts "\n\n"
+      puts @@description
+  end
+
+  def self.error_catch
+
+  end
 end
+
+# p WikiSearch.link("dev bootcamp")  ##### CODE BEGINS HERE
 # Refactor and pseudocode
 # Parses wiki page requested by user
-def search_wiki
-  try_again = false
-  puts "What would you like to search on Wikipedia?"
-  search = gets.chomp
-
-  new_str = []
-  search.each_char do |chr|
-    if chr == " "
-      chr = "_"
-    end
-    new_str << chr
-    end
-
-  system "clear"
-  begin
-  page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/" + "#{new_str.join}"))
-  # p page.class
-  header = page.css("h1")[0].text
-
-  # Prints one character at a time on the same line...just to be a little fancy!
-  header.each_char do |chr|
-    print chr
-    sleep(0.1)
-    STDOUT.flush
-  end
-
-  puts "\n\n"
-  puts page.css("p")[0].text
-
-# Handles 404 error
-  rescue OpenURI::HTTPError => error
-    if error.message == '404 Not Found'
-      puts "This page does not exist. Please try again."
-      try_again = true
-      search_wiki
-    else
-      raise error
-    end
-  end
-  if try_again == false
-    puts "\nWould you like to search something else?(y/n)"
-    search_again = gets.chomp
-    until search_again == "y" || search_again == "n"
-      puts "Please enter 'y' or 'n'."
-      search_again = gets.chomp
-    end
-    if search_again == "y"
-      puts "\n"
-      search_wiki
-    end
-  end
-end
+# def search_wiki
+#   system "clear"
+#   begin
+#   page = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/" + "#{new_str.join}"))
+#   # p page.class
+#   header = page.css("h1")[0].text
+#
+#   # Prints one character at a time on the same line...just to be a little fancy!
+#   header.each_char do |chr|
+#     print chr
+#     sleep(0.1)
+#     STDOUT.flush
+#   end
+#
+#   puts "\n\n"
+#   puts page.css("p")[0].text
+#
+# # Handles 404 error
+#   rescue OpenURI::HTTPError => error
+#     if error.message == '404 Not Found'
+#       puts "This page does not exist. Please try again."
+#       try_again = true
+#       search_wiki
+#     else
+#       raise error
+#     end
+#   end
+#   if try_again == false
+#     puts "\nWould you like to search something else?(y/n)"
+#     search_again = gets.chomp
+#     until search_again == "y" || search_again == "n"
+#       puts "Please enter 'y' or 'n'."
+#       search_again = gets.chomp
+#     end
+#     if search_again == "y"
+#       puts "\n"
+#       search_wiki
+#     end
+#   end
+# end
 
 # search_wiki
